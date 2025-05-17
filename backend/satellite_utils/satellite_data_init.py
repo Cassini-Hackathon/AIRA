51f9d95c629ea89902502b9d91625ee2cc88d7fb
alessandromarcellini <alessandromarcellini49@gmail.com>
2025-05-17 21:29:34 +0200

    files setup for apis calls at startup and refresh

---
+++ backend/satellite_utils/satellite_data_init.py
@@ -0,0 +1,24 @@
+# functions to fetch the data from the satellites at startup
+import os
+
+
+#apis to fetch:
+apis = [
+    {
+
+    },
+    {
+
+    },
+]
+
+#for each api fetch the data
+def load_apis():
+    for api in apis:
+        pass
+
+
+def save_data(data_set_name, data, path):
+    pass
+
+
